65debaf6cf86e3c30cb4e5272b05f3fe322ec1c6
Tom Van Mele <vanmelet@ethz.ch>
2018-12-03 13:54:25 +0100

    convert stuff to blocks from Rhino

---
+++ scripts/meshestoblocks.py
@@ -0,0 +1,10 @@
+import compas
+import compas_rhino
+import compas_rbe
+
+from compas_rbe.datastructures import Assembly
+
+guids = compas_rhino.select_meshes()
+assembly = Assembly.from_meshes(guids)
+
+assembly.to_json(compas_rbe.get('gnene_vault_blocks.json'))
