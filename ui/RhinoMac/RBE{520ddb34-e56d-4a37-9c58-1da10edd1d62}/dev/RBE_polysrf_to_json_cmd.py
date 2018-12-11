b427ed257e6886381d6c86902e1e258f539c1739
GeneKao <kao.gene@gmail.com>
2018-12-11 01:21:45 +0100

    Add command to save polysrf to assembly json

---
+++ ui/RhinoMac/RBE{520ddb34-e56d-4a37-9c58-1da10edd1d62}/dev/RBE_polysrf_to_json_cmd.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import os
+import sys
+
+import rhinoscriptsyntax as rs
+import scriptcontext as sc
+
+import Rhino
+
+import compas
+import compas_rhino
+import compas_rbe
+from compas_rbe.datastructures import Assembly
+
+__commandname__ = "RBE_polysrf_to_json"  # => the part before _cmd.py
+
+
+def RunCommand(is_interactive):
+
+    if not 'RBE' in sc.sticky:
+        raise Exception('Initialise RBE first!')
+    RBE = sc.sticky['RBE']
+
+    try:
+
+        assembly = Assembly()
+
+        guids = compas_rhino.select_surfaces()
+        my_assembly = assembly.add_blocks_from_polysurfaces(guids)
+
+        filename = rs.SaveFileName(
+            "Save",
+            filter='JSON files (*.json)|*.json||',
+            folder=compas_rbe.DATA)
+
+        assembly.to_json(filename)
+
+    except Exception as error:
+
+        print(error)
